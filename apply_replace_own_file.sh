6152ea9783be8b4ff2323c8e26beee749f72a42c
Arvin <arvinxwang@gmail.com>
2021-05-26 18:45:52 +0800

    improve
    Signed-off-by: Arvin

--- apply_replace_own_file.sh
+++ apply_replace_own_file.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 
 echo "cp -f /jd/own/imwcc_jd_imwcc/jd_factory.js /jd/own/hapecoder_JD-SCRIPT"
+
 cp -f /jd/own/imwcc_jd_imwcc/jd_factory.js /jd/own/hapecoder_JD-SCRIPT
+
+
+echo "force update scripts"
+cp -f  /jd/own/imwcc_jd_imwcc/jd_syj.js  /jd/scripts
+cp -f /jd/own/imwcc_jd_imwcc/jd_city.js  /jd/scripts
+cp -f /jd/own/imwcc_jd_imwcc/jd_try.js   /jd/scripts
--- hang_up_task.sh
+++ hang_up_task.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 
-function check_hang_task(){
-  count=`ps -ef |grep $1 |grep -v "grep" |wc -l`
-  #echo $count
-  if [ 0 == $count ];then
-    jtask $1 &
-  fi
-}
-
-check_hang_task /jd/scripts/jd_crazy_joy_coin.js
-
+echo "check jd_crazy_joy_coin"
+count=`ps -ef |grep /jd/scripts/jd_crazy_joy_coin.js |grep -v "grep" |wc -l`
+#echo $count
+if [ 0 == $count ];then
+    echo "run jd_crazy_joy_coin"
+    otask /jd/own/hapecoder_JD-SCRIPT/jd_crazy_joy_coin.js &
+fi
