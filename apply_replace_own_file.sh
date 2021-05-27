91af09b896fecba3d4f8ec825bba2e1cdef0295b
arvin <arvinxwang@gmail.com>
2021-05-27 22:11:55 +0800

    init jshare.sh

--- apply_replace_own_file.sh
+++ apply_replace_own_file.sh
@@ -9,3 +9,4 @@ echo "force update scripts"
 cp -f  /jd/own/imwcc_jd_imwcc/jd_syj.js  /jd/scripts
 # cp -f /jd/own/imwcc_jd_imwcc/jd_city.js  /jd/scripts
 cp -f /jd/own/imwcc_jd_imwcc/jd_try.js   /jd/scripts
+cp -f /jd/own/imwcc_jd_imwcc/jshare.sh  /jd
