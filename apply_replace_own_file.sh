4538cb5eb3c904d9cfff1a862b5b9ee8bf3ed80d
Arvin <arvinxwang@gmail.com>
2021-05-27 17:50:55 +0800

    remove own jd_city
    Signed-off-by: Arvin

--- apply_replace_own_file.sh
+++ apply_replace_own_file.sh
@@ -7,5 +7,5 @@ cp -f /jd/own/imwcc_jd_imwcc/jd_factory.js /jd/own/hapecoder_JD-SCRIPT
 
 echo "force update scripts"
 cp -f  /jd/own/imwcc_jd_imwcc/jd_syj.js  /jd/scripts
-cp -f /jd/own/imwcc_jd_imwcc/jd_city.js  /jd/scripts
+# cp -f /jd/own/imwcc_jd_imwcc/jd_city.js  /jd/scripts
 cp -f /jd/own/imwcc_jd_imwcc/jd_try.js   /jd/scripts
