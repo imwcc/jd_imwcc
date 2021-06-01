205a112b7623e896ab12fe253d8afabf17e371f8
Arvin <arvinxwang@gmail.com>
2021-06-01 15:22:26 +0800

    add main shell
    Signed-off-by: Arvin

--- main_own_change.sh
+++ main_own_change.sh
@@ -5,3 +5,6 @@ work_dir=/jd/own/imwcc_jd_imwcc
 . $work_dir/custom_code.sh
 . $work_dir/apply_default_help_code.sh
 . $work_dir/hang_up_task.sh
+
+pip3 install -r $work_dir/pythonProjects/JD_tencent_scf_to_jd_scripts/requirements.txt
+python3 $work_dir/pythonProjects/JD_tencent_scf_to_jd_scripts/main.py
