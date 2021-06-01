2584b5a22b5cd22dc95e9708022c5c6991011b30
Arvin <arvinxwang@gmail.com>
2021-06-01 15:18:52 +0800

    add support autoAdd script for JD_tencent_scf
    Signed-off-by: Arvin

--- pythonProjects/JD_tencent_scf_to_jd_scripts/main.py
+++ pythonProjects/JD_tencent_scf_to_jd_scripts/main.py
@@ -2,12 +2,13 @@ import json
 import os
 import demjson
 import socket
+import time
 
 import logging
 logging.basicConfig(format='%(asctime)s - %(pathname)s[line:%(lineno)d] - %(levelname)s: %(message)s',
                     level=logging.DEBUG)
 
-FILE_DIR = os.path.abspath(__file__)
+FILE_DIR = os.path.dirname(os.path.abspath(__file__))
 print(FILE_DIR)
 # own 目录下面有三种目录优先级:
 #   1. scripts 大佬目录
@@ -24,23 +25,30 @@ if HOST_NAME == 'arvin-wang':
     ROOT_DIR='/home/arvin/code'
     JD_tencent_scf_dir = os.path.join(ROOT_DIR, 'JD_tencent_scf')
     parse_from_file = os.path.join(ROOT_DIR, 'JD_tencent_scf/jd_task.json')
-    scripts_crontab_config = os.path.join(ROOT_DIR, 'jd_scripts/docker/crontab_list.sh')
+    scripts_crontab_config = os.path.join(FILE_DIR, 'test_files/crontab.list')
     scripts_jd_imwcc_dir = os.path.join(ROOT_DIR, 'jd_imwcc')
 
     exclude_file_list = ['jd_cash_exchange.js']
-
     result_crontab_file = os.path.join(JD_tencent_scf_dir, 'JD_tencent_crontab.sh')
-
 
 elif HOST_NAME == 'jd-arvin':
     ROOT_DIR='/jd'
     JD_tencent_scf_dir = os.path.join(ROOT_DIR, 'zero205_JD_tencent_scf')
     parse_from_file = os.path.join(ROOT_DIR, 'zero205_JD_tencent_scf/jd_task.json')
-    scripts_crontab_config = os.path.join(ROOT_DIR, 'scripts/docker/crontab_list.sh')
+    scripts_crontab_config = os.path.join(ROOT_DIR, 'config/crontab.list')
     scripts_jd_imwcc_dir = os.path.join(ROOT_DIR, 'own/imwcc_jd_imwcc')
 
     exclude_file_list = ['jd_cash_exchange.js']
+    result_crontab_file = '/jd/config/crontab.list'
 
+elif HOST_NAME == 'ubuntu157362':
+    ROOT_DIR='/home/arvin/code'
+    JD_tencent_scf_dir = os.path.join(ROOT_DIR, 'JD_tencent_scf')
+    parse_from_file = os.path.join(ROOT_DIR, 'JD_tencent_scf/jd_task.json')
+    scripts_crontab_config = os.path.join(FILE_DIR, 'test_files/crontab.list')
+    scripts_jd_imwcc_dir = os.path.join(ROOT_DIR, 'jd_imwcc')
+
+    exclude_file_list = ['jd_cash_exchange.js']
     result_crontab_file = '/jd/config/crontab.list'
 
 else:
@@ -51,7 +59,7 @@ else:
 if __name__ == '__main__':
     jd_scripts_crontab_list = []
 
-    # 解析大佬的 jd_scripts 目录配置文件:
+    # 解析大佬的 docker 默认 crontab 配置文件:
     with open(scripts_crontab_config, 'r') as f:
         for line in f.readlines():
             line = line.strip().replace('\n', '')
@@ -59,10 +67,11 @@ if __name__ == '__main__':
                 continue
             elif line.startswith('#'):
                 continue
-            elif '.js' not in line:
+            elif '.js' not in line and 'jtask' not in line:
                 continue
             jd_scripts_crontab_list.append(line)
 
+    # 最终扫描出来缺少的脚本 crontab + 注释
     result_crontab_list = []
 
     with open(parse_from_file, 'r') as f:
@@ -78,10 +87,13 @@ if __name__ == '__main__':
                 is_in_jd_scripts = False
                 # 判斷是否在 已經jd_scripts 配置中， 若是 退出
                 for jd_scripts_line_item in jd_scripts_crontab_list:
-                    if file_name in jd_scripts_line_item:
+                    print(str(file_name).split('.js')[0])
+                    print(jd_scripts_line_item)
+                    if str(file_name).split('.js')[0] in str(jd_scripts_line_item):
                         is_in_jd_scripts = True
                         logging.info("{} 已经存在于 {}".format(file_name, jd_scripts_line_item))
                         break
+
                 if is_in_jd_scripts:
                     continue
 
@@ -98,33 +110,12 @@ if __name__ == '__main__':
                     result_crontab_list.append(crontab_item)
 
 
-    # 对比不存在 list
-    final_result_list = []
+    if len(result_crontab_list) > 0:
+        current_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+        with open(scripts_crontab_config, 'a+') as f:
+            f.write("# =========== JD_tencent_scf start {}  ==============\n".format(current_time))
+            for line in result_crontab_list:
+                logging.info("写入: " + line)
+                f.write(line + '\n')
+            f.write("# =========== JD_tencent_scf end {} ==============\n\n".format(current_time))
 
-    with open(parse_from_file, 'r') as f:
-        result = f.readlines()
-
-        for parse_from_file_line in result:
-            is_in_old_crontab_list = False
-            count = 0
-            for new_item in result_crontab_list:
-                count += 1
-                if new_item.startswith('#'):
-                    continue
-                if new_item in parse_from_file_line:
-                    is_in_old_crontab_list = True
-
-            if not is_in_old_crontab_list:
-                print(count)
-                print(result_crontab_list)
-                final_result_list.append(result_crontab_list[count-1])
-                final_result_list.append(result_crontab_list[count])
-
-    print(final_result_list)
-    with open(parse_from_file, 'a+') as f:
-        for line in final_result_list:
-            f.write(line + '/n')
-
-
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
