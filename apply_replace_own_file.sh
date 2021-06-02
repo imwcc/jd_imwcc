afc052a42bcc652e4e7c783ec1146632ab16e077
Arvin <arvinxwang@gmail.com>
2021-06-02 13:59:42 +0800

    fix path issue
    Signed-off-by: Arvin

--- apply_replace_own_file.sh
+++ apply_replace_own_file.sh
@@ -4,12 +4,12 @@ echo "cp -f /jd/own/imwcc_jd_imwcc/jd_factory.js /jd/own/hapecoder_JD-SCRIPT"
 
 cp -f /jd/own/imwcc_jd_imwcc/jd_factory.js /jd/own/hapecoder_JD-SCRIPT
 
+cp -f /jd/own/imwcc_jd_imwcc/jshare.sh  /jd
 
 echo "force update scripts"
 cp -f  /jd/own/imwcc_jd_imwcc/jd_syj.js  /jd/scripts
 # cp -f /jd/own/imwcc_jd_imwcc/jd_city.js  /jd/scripts
 cp -f /jd/own/imwcc_jd_imwcc/jd_try.js   /jd/scripts
-cp -f /jd/own/imwcc_jd_imwcc/jshare.sh  /jd
-cp -f  /jd/own/imwcc_jd_imwcc/jd_bean_change.js /jd
-cp -f /jd/own/imwcc_jd_imwcc/jd_pk.js /jd
+cp -f  /jd/own/imwcc_jd_imwcc/jd_bean_change.js /jd/scripts
+cp -f /jd/own/imwcc_jd_imwcc/jd_pk.js /jd/scripts
 
