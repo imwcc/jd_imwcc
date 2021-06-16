fbe1b630343b3f6ff3fa37f143b557239a053731
arvin <arvinxwang@gmail.com>
2021-06-16 21:05:27 +0800

    jd_bean_change remove

--- apply_replace_own_file.sh
+++ apply_replace_own_file.sh
@@ -10,6 +10,6 @@ echo "force update scripts"
 cp -f  /jd/own/imwcc_jd_imwcc/jd_syj.js  /jd/scripts
 # cp -f /jd/own/imwcc_jd_imwcc/jd_city.js  /jd/scripts
 cp -f /jd/own/imwcc_jd_imwcc/jd_try.js   /jd/scripts
-cp -f  /jd/own/imwcc_jd_imwcc/jd_bean_change.js /jd/scripts
+#cp -f  /jd/own/imwcc_jd_imwcc/jd_bean_change.js /jd/scripts
 cp -f /jd/own/imwcc_jd_imwcc/jd_pk.js /jd/scripts
 
