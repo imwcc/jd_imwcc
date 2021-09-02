3629725a940072e85a12fcb624f048c6ebb168bd
Arvin <arvinxwang@gmail.com>
2021-09-02 12:01:55 +0800

    fix
    Signed-off-by: Arvin

--- pythonProjects/ck_manager/cookie.py
+++ pythonProjects/ck_manager/cookie.py
@@ -5,6 +5,8 @@
 # @Project : jd_scripts
 # @Desc    :
 import json
+import os
+import time
 from urllib.parse import urlencode
 
 import requests
@@ -26,10 +28,13 @@ def ws_key_to_pt_key(pt_pin, ws_key):
         'user-agent': 'okhttp/3.12.1;jdmall;android;version/10.1.2;build/89743;screen/1080x2293;os/11;network/wifi;',
         'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
     }
-    url = 'https://api.m.jd.com/client.action?functionId=genToken&clientVersion=10.1.2&build=89743&client=android' \
-          '&d_brand=&d_model=&osVersion=&screen=&partner=&oaid=&openudid=a27b83d3d1dba1cc&eid=&sdkVersion=30&lang' \
-          '=zh_CN&uuid=a27b83d3d1dba1cc&aid=a27b83d3d1dba1cc&area=19_1601_36953_50397&networkType=wifi&wifiBssid=&uts' \
-          '=&uemps=0-2&harmonyOs=0&st=1630413012009&sign=ca712dabc123eadd584ce93f63e00207&sv=121'
+    url = "https://api.m.jd.com/client.action?functionId=genToken&clientVersion=10.1.0" \
+          "&build=89583&client=android&d_brand=&d_model=&osVersion=&screen=2208*1080&partner=&" \
+          "oaid=&eid=&sdkVersion=&lang=zh_CN" \
+          "&uuid=1d8898264ebf4c20&aid=1d8898264ebf4c20&area=22_1930_50949_60258" \
+          "&networkType=wifi&wifiBssid=d12a91a7eebc7a8428e10cce118d2142&uts=" \
+          "&uemps=0-2&harmonyOs=0&st=1630551241407&sign=fac11195ffedbac8e9cbb26a3855b891&sv=122"
+
     body = 'body=%7B%22to%22%3A%22https%253a%252f%252fplogin.m.jd.com%252fjd-mlogin%252fstatic%252fhtml' \
            '%252fappjmp_blank.html%22%7D&'
     response = requests.post(url, data=body, headers=headers, cookies=cookies, verify=False)
@@ -50,6 +55,7 @@ def ws_key_to_pt_key(pt_pin, ws_key):
             return 'pt_key={};pt_pin={};'.format(v, pt_pin)
     return None
 
+
 def sync_check_cookie(cookies):
     """
     检测cookies是否过期
@@ -59,8 +65,8 @@ def sync_check_cookie(cookies):
     try:
         url = 'https://api.m.jd.com/client.action?functionId=newUserInfo&clientVersion=10.0.9&client=android&openudid' \
               '=a27b83d3d1dba1cc&uuid=a27b83d3d1dba1cc&aid=a27b83d3d1dba1cc&area=19_1601_36953_50397&st' \
-              '=1626848394828&sign=447ffd52c08f0c8cca47ebce71579283&sv=101&body=%7B%22flag%22%3A%22nickname%22%2C' \
-              '%22fromSource%22%3A1%2C%22sourceLevel%22%3A1%7D&'
+              '={}&sign=447ffd52c08f0c8cca47ebce71579283&sv=&body='.format(int(time.time() * 1000))
+
         headers = {
             'user-agent': 'okhttp/3.12.1;jdmall;android;version/10.0.9;build/89099;screen/1080x2293;os/11;network/wifi;'
         }
@@ -73,3 +79,10 @@ def sync_check_cookie(cookies):
     except Exception as e:
         print(e.args)
         return False
+
+
+if __name__ == '__main__':
+    pt_pin = os.getenv('pt_pin')
+    ws_key = os.getenv('wskey')
+    result = ws_key_to_pt_key(pt_pin, ws_key)
+    print(result)
