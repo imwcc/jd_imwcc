d54c17e0485377686c6b1765de4b5445d9697a3b
Arvin <arvinxwang@gmail.com>
2021-10-27 13:05:40 +0800

    fix
    Signed-off-by: Arvin

--- pythonProjects/rull_all_task.py
+++ pythonProjects/rull_all_task.py
@@ -15,7 +15,7 @@ FILE_DIR = os.path.dirname(os.path.abspath(__file__))
 HOST_NAME = socket.gethostname()
 DEBUG = 'jd-arvin' not in HOST_NAME
 
-str_crontab = os.popen("crontab -l").readlines()
+str_crontab = os.popen("crontab -l").read()
 if DEBUG:
     with open("/home/arvin/crontab.list", 'r') as f:
         str_crontab = f.read()
@@ -30,7 +30,7 @@ for line in str_crontab.split('\n'):
 
     if 'otask' in line:
         print(line.split('otask')[-1])
-        global_crontab_task.append("otask {}" + line.split('otask')[-1])
+        global_crontab_task.append("otask {}".format(line.split('otask')[-1]))
         count += 1
 
 logging.info("共计 {} task".format(count))
@@ -43,9 +43,9 @@ def func(cmd):
 
 
 if __name__ == "__main__":
-    pool = multiprocessing.Pool(processes=44)
+    pool = multiprocessing.Pool(processes=34)
     for i in range(count):
-        pool.apply(func, (global_crontab_task[i],))  # 维持执行的进程总数为processes，当一个进程执行完毕后会添加新的进程进去
+        pool.apply_async(func, (global_crontab_task[i],))  # 维持执行的进程总数为processes，当一个进程执行完毕后会添加新的进程进去
 
     pool.close()
     pool.join()  # 调用join之前，先调用close函数，否则会出错。执行完close后不会有新的进程加入到pool,join函数等待所有子进程结束
